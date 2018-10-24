d5f275c2925b14ef788f431d2e1ab77f95e626b8
Carolin Ivens <carolin.ivens@gmail.com>
2018-10-24 13:40:47 +0000

    final

--- src/LikedMovies.js
+++ src/LikedMovies.js
@@ -1,22 +1,17 @@
 import React from 'react';
 
 function LikedMovies(props) {
-	 const likedMovies = props.profiles.map(profile => {
-      return props.movies[profile.favoriteMovieID].id
-    });
-    const numMovies = Object.keys(props.movies).map(movie => {
-      return likedMovies.filter(likedMovie => likedMovie === movie).length
-    });
-   const rows = [];
-  for(let i = 1; i <= props.movies.length; i++) {
-    if(numMovies[i] === 0) {
-      rows.push(<p>None of the current users liked this movie</p>);
+	 const numLikers = props.profiles.filter(profile => profile.favoriteMovieID == props.movie).length;
+    
+   
+    if(numLikers === 0) {
+      return (<p>None of the current users liked this movie</p>);
     }
     else {
-      rows.push(<p>Liked By: {numMovies[i]}</p>);
+      return (<p>Liked By: {numLikers}</p>);
     }
-  }
-  return rows;
+  
+  
 }
 
 export default LikedMovies;
